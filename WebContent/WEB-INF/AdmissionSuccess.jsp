1e4d93ea1c080c7f138d04863f9c06da4eb0c6b6
adn201213 <adn201213@hotmail.com>
2020-07-03 20:37:45 -0400

    user define
    path variable

--- WebContent/WEB-INF/AdmissionSuccess.jsp
+++ WebContent/WEB-INF/AdmissionSuccess.jsp
@@ -1,18 +1,42 @@
 <html>
 <body>
-	<h2>Congratulations!! the Engineering college has processed your Application form successfully</h2>
+	<h1>${headerMessage}</h1>
  
-	
-	<h2>Details submitted by you</h2>
- <table border=1>
- <tr>
- <td>student Name</td>
- <td>${student.studentName}</td>
- </tr>
-  <tr>
- <td>student Hobby</td>
- <td>${student.hobby}</td>
- </tr>
- </table>
+	<h3>Congratulations!! the Engineering college has processed your Application form successfully</h3>
+
+	<h2>Details submitted by you:: </h2>
+
+	<table>
+		<tr>
+			<td>Student Name :</td>
+			<td>${student1.studentName}</td>
+		</tr>
+		<tr>
+			<td>Student Hobby :</td>
+			<td>${student1.studentHobby}</td>
+		</tr>
+		<tr>
+			<td>Student Mobile :</td>
+			<td>${student1.studentMobile}</td>
+		</tr>
+
+		<tr>
+			<td>Student DOB :</td>
+			<td>${student1.studentDOB}</td>
+		</tr>
+		<tr>
+			<td>Student Skills :</td>
+			<td>${student1.studentSkills}</td>
+		</tr>
+		<tr>
+			<td>Student Address :</td>
+			<td>country: ${student1.studentAddress.country}
+			       city: ${student1.studentAddress.city}
+			     street: ${student1.studentAddress.street}
+			    pincode: ${student1.studentAddress.pincode}</td>
+		</tr>
+
+	</table>
+
 </body>
 </html>
--- src/com/gontuseries/studentadmissioncontroller/StudentAdmissionController.java
+++ src/com/gontuseries/studentadmissioncontroller/StudentAdmissionController.java
@@ -1,56 +1,43 @@
 package com.gontuseries.studentadmissioncontroller;
-
-
-
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StudentAdmissionController {
 
+
 	@RequestMapping(value="/admissionForm.html", method = RequestMethod.GET)
 	public ModelAndView getAdmissionForm() {
 
-		ModelAndView model = new ModelAndView("AdmissionForm");
-
-		return model;
+		ModelAndView model1 = new ModelAndView("AdmissionForm");
+		
+		return model1;
 	}
-	/*
+
+	@ModelAttribute
+    public void addingCommonObjects(Model model1) {
+		
+		model1.addAttribute("headerMessage", "Gontu College of Engineering, India");
+	}
+
 	@RequestMapping(value="/submitAdmissionForm.html", method = RequestMethod.POST)
-	public ModelAndView submitAdmissionForm(@RequestParam("studentName") String name, @RequestParam("studentHobby") String hobby) {
-        Student student=new Student();
-        student.setFirstName(name);
-        student.setHobby(hobby);
-		ModelAndView model = new ModelAndView("AdmissionSuccess");
-		model.addObject("headMessage","Details submitted by you:: Name: "+name+ ", Hobby: " + hobby);
-        model.addObject("student",student);
-		return model;
+	public ModelAndView submitAdmissionForm(@ModelAttribute("student1") Student student, BindingResult result) {
+		
+		 if (result.hasErrors()) {
+
+				ModelAndView model1 = new ModelAndView("AdmissionForm");
+				return model1;
+		 }
+
+		ModelAndView model1 = new ModelAndView("AdmissionSuccess");
+		return model1;
 	}
-*/
-	//@RequestMapping(value="/submitAdmissionForm.html", method = RequestMethod.POST)
-	//public ModelAndView submitAdmissionForm(@RequestParam Map<String,String> reqPar) {
-
-	///	String name = reqPar.get("studentName");
-		//String hobby = reqPar.get("hobby");
-
-		//ModelAndView model = new ModelAndView("AdmissionSuccess");
-	//	model.addObject("msg","Details submitted by you:: Name: "+name+ ", Hobby: " + hobby);
-
-	//	return model;
-	//}
 	
-	@RequestMapping(value="/submitAdmissionForm.html", method = RequestMethod.POST)
-	public ModelAndView submitAdmissionForm(@ModelAttribute("student") Student student) {
-     
-		ModelAndView model = new ModelAndView("AdmissionSuccess");
-	//	model.addObject("headMessage","Details submitted by you:");
-       
-		return model;
-	}
 }
+
